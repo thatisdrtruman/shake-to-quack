631070db48f29ec260d1da5ae76f1fbb1a8b122a
thatisdrtruman <joe.liu2nd@gmail.com>
2024-06-14 02:11:34 +0100

    WIP version 1

---
+++ README.md
@@ -0,0 +1,8 @@
+Shake to Quack
+based on Tokyo Disneyland 2024 Quack Stick, written in Godot 3.5.3 with C# Support
+
+Quack Stick Duck image copyright of Disney, all rights reserved. 
+
+Sound: ""Tokyo Disneyland Haul: Donald Duck's Quacky City Merch!" by Wandering with Manami: Tokyo Disney Travel Guide found at https://www.youtube.com/watch?v=vrXCYUOBNK4
+
+Work in progress
---
+++ scene.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class scene : Node2D
+{
+    // ToAdd:
+    // play-button pressed: shake_frame animation appears, play random sounds in the sounds folder in a loop,
+    // stop when button released, and shake_frame dissapears
+
+    // Mobile accelerometer support. As 'play-button pressed' but when shaking the device. 
+
+    public override void _Ready()
+    {
+
+    }
+
+ 
+}
